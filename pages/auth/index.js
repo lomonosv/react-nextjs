80fd24f16919a5b950c481936f3e2cecb82bd05f
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-10-04 10:26:16 +0300

    Special lifecycle hook usage

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
 	<div>
-		<h1>The Auth Index Page</h1>
+		<h1>The Auth Index Page - { props.appName }</h1>
 		<User name="Vladimir" age={ 30 }/>
 	</div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+	console.log(context);
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve({ appName: 'Supper App Auth' })
+		}, 1000);
+	});
+
+	return promise;
+};
 
 export default authIndexPage;
